9ead9d75e6a888f09095f7361e91d8840043dabf
EriNumajiri <s1322114@cst.otsuma.ac.jp>
2025-10-07 00:01:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Unity からのアクセスを許可
 
 @app.route('/')
 def hello():
-    return "Hello, Render!"
+    # Unity に送りたい文字列（自由に変更OK）
+    return "jump"
 
 if __name__ == '__main__':
     app.run(debug=True)
